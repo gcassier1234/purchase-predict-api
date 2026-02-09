1567d83f87121366b4ab3072e2e88f6b9e253020
gabriel <gcassier1234@gmail.com>
2026-02-09 22:58:40 +0100

    code ready for staging branch

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import requests
 dataset = pd.read_csv("data_test/primary.csv")
 dataset = dataset.drop(["user_session", "user_id", "purchased"], axis=1)
 
-rep = requests.post("http://34.22.155.184/predict", json=dataset.sample(n=10).to_json()).json()
+rep = requests.post("https://purchase-predict-api-2-526969011890.europe-west9.run.app/predict", json=dataset.sample(n=10).to_json()).json()
 
 print(rep)
